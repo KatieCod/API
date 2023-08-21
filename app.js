0537523df93d7ee022cbf387ab9272f545712139
Katie <e.v.rassolenko@gmail.com>
2023-08-21 22:39:28 +0300

    fixing bug

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var productsRouter = require('./routes/products');
+var bcrypt = require('bcrypt'); 
+const cors = require('cors')
 
 var app = express();
 
-const cors = require('cors')
-app.use(cors())
+app.use(cors(
+  {origin: ['http://localhost:3000'],
+  methods: ['POST', 'GET'],
+  credentials: true}
+));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,7 +26,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
